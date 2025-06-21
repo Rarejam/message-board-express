3ca57a405321ff02095ecb69792600d570f7e244
Jamal Rarejam <agarajamaldeen@gmail.com>
2025-06-20 21:03:05 -0400

    pool.js changes

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,10 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 const pool = new Pool({
-  // host: process.env.HOST,
-  // user: process.env.USER,
-  // password: process.env.PASSWORD,
-  // port: process.env.DEFAULTPORT,
-  // database: process.env.DATABASE,
-   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Render requires this
-  },
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.DEFAULTPORT,
+  database: process.env.DATABASE,
 });
 module.exports = pool;
